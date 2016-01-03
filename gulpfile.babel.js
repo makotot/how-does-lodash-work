7d79494aa84615a0cf50d732559147e768ea3a26
makotot <makoto.ttn@gmail.com>
2016-01-04 00:44:44 +0900

    * task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 import gulp from 'gulp'
 import del from 'del'
+import runSequence from 'run-sequence'
+import browserSync from 'browser-sync'
 
 
-gulp.task('clean', () => {
+gulp.task('clean', (done) => {
   del(['./dist']).then(() => {
     done()
   })
@@ -13,4 +15,18 @@ gulp.task('template', () => {
   return gulp
     .src(['./src/*.html'])
     .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('serve', () => {
+  runSequence('clean', ['template'], () => {
+    browserSync.init({
+      server: './dist',
+      open: false
+    })
+  })
+})
+
+gulp.task('build', () => {
+  runSequence('clean', ['template'], () => {
+  })
 })
