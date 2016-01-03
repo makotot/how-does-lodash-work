833f1451208c78505efb40637f7e7bc92173dbe6
makotot <makoto.ttn@gmail.com>
2016-01-03 21:43:08 +0900

    * task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp'
 import del from 'del'
 
+
 gulp.task('clean', () => {
   del(['./dist']).then(() => {
     done()
   })
+})
+
+gulp.task('template', () => {
+
+  return gulp
+    .src(['./src/*.html'])
+    .pipe(gulp.dest('./dist/'))
 })
